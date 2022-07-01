970a98a866270b567c2bd8f73d01fdf456ae008c
cs-mas <masako.gray@clearscore.com>
2022-07-01 11:35:43 +0100

    creating second Activity page so FindFriend button can be linked

---
+++ app/src/main/java/com/masgray/findfriends/FindFriendsActivity.kt
@@ -0,0 +1,12 @@
+package com.masgray.findfriends
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FindFriendsActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard_fragment)
+    }
+}
